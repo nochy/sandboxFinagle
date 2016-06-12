d4d54ce6e4e13b3b3c4c3364fd0f76d4a98fd50d
nochy <radius.t.n@gmail.com>
2016-06-13 01:15:38 +0900

    init

---
+++ minimal-client/src/main/scala/sandbox/finagle/client/MinimalClient.scala
@@ -0,0 +1,17 @@
+package sandbox.finagle.client
+
+import com.twitter.finagle.{Http, Service}
+import com.twitter.finagle.http
+import com.twitter.util.{Await, Future}
+
+object MinimalClient extends App {
+
+  val client: Service[http.Request, http.Response] = Http.newService("www.scala-lang.org:80")
+  val request  = http.Request(http.Method.Get, "/")
+  request.host = "www.scala-lang.org"
+  val response: Future[http.Response] = client(request)
+  Await.result(response.onSuccess { rep: http.Response =>
+    println("GET success: " + rep)
+  })
+
+}
